d158b9c97224e3945099320bc532c6e6869dab09
Richard Amerman <richard@amerman.net>
2026-01-21 22:28:48 -0800

    chore: Rebuilt Rust debug target artifacts, clearing many old build files and updating metadata.

--- rust/pocket-tts-ios/target/debug/deps/uniffi_bindgen-6caf8fe7c79a6363.d
+++
@@ -1,5 +0,0 @@
-/Users/ramerman/dev/unamentis/rust/pocket-tts-ios/target/debug/deps/uniffi_bindgen-6caf8fe7c79a6363.d: src/bin/uniffi_bindgen.rs
-
-/Users/ramerman/dev/unamentis/rust/pocket-tts-ios/target/debug/deps/libuniffi_bindgen-6caf8fe7c79a6363.rmeta: src/bin/uniffi_bindgen.rs
-
-src/bin/uniffi_bindgen.rs:

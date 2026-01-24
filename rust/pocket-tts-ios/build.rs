35df76f87f4e44cbf3a1acbab9cc6fe211e77f98
Richard Amerman <richard@amerman.net>
2026-01-23 22:13:52 -0800

    refactor: remove the `pocket-tts-ios` Rust project and update `test-hooks.ts`.

--- rust/pocket-tts-ios/build.rs
+++
@@ -1,7 +0,0 @@
-//! Build script for pocket-tts-ios
-//!
-//! Generates UniFFI scaffolding from the UDL file.
-
-fn main() {
-    uniffi::generate_scaffolding("src/pocket_tts.udl").unwrap();
-}
--- rust/pocket-tts-ios/src/bin/uniffi_bindgen.rs
+++
@@ -1,7 +0,0 @@
-//! UniFFI bindgen CLI
-//!
-//! Generates Swift bindings from the UDL interface definition.
-
-fn main() {
-    uniffi::uniffi_bindgen_main()
-}
